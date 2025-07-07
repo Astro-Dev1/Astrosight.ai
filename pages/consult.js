f79c58d2f5d166c5f837aef72c36566714510c99
astrosight-ai <astrosight.ai@gmail.com>
2025-07-07 18:42:07 +0530

    new seo updates

---
+++ pages/consult.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ConsultPage = () => {
+  return (
+    <div>
+      <h1>Consult</h1>
+      <p>Consultation page coming soon...</p>
+    </div>
+  );
+};
+
+export default ConsultPage;
---
+++ pages/shop.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ShopPage = () => {
+  return (
+    <div>
+      <h1>Shop</h1>
+      <p>Shop page coming soon...</p>
+    </div>
+  );
+};
+
+export default ShopPage;
