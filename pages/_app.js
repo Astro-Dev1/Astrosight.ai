2c70533aba5b2e593914836da1f2caefd7288255
astrosight-ai <astrosight.ai@gmail.com>
2025-07-07 18:59:26 +0530

    new seo updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,10 @@
 import '../styles/globals.css';
 // import '../styles/ScrollingCardUI.css';
-import { SessionProvider } from "next-auth/react";
 import Head from 'next/head';
 
-function MyApp({ Component, pageProps: { session, ...pageProps } }) {
+function MyApp({ Component, pageProps }) {
   return (
-    <SessionProvider session={session}>
+    <>
       <Head>
         {/* Google tag (gtag.js) - Added to all pages via _app.js */}
         <script async src="https://www.googletagmanager.com/gtag/js?id=AW-17273163672"></script>
@@ -19,7 +18,7 @@ function MyApp({ Component, pageProps: { session, ...pageProps } }) {
         }}></script>
       </Head>
       <Component {...pageProps} />
-    </SessionProvider>
+    </>
   );
 }
 
