cca586c499db71752445b1d92d6e799628a0f94a
Astro-Dev1 <astrosight.ai@gmail.com>
2025-09-12 14:16:01 +0530

    latest

--- components/CustomHeader.jsx
+++ components/CustomHeader.jsx
@@ -9,6 +9,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
 const CustomHeader = ({ isHomePage = false }) => {
+  console.log(isHomePage)
   const router = useRouter();
   const [isMobile, setIsMobile] = useState(true);
   const [sideMenuOpen, setSideMenuOpen] = useState(false);
